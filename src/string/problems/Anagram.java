cc05cc6683d515a44d2254a2b2fef69680676957
Vlad <vp1192@gmail.com>
2022-01-05 05:29:38 -0500

    anagram

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,13 +1,40 @@
 package string.problems;
 
+import java.util.Arrays;
+import java.util.Locale;
+
 /**
  * Created by mrahman on 04/22/17.
  */
 public class Anagram {
 
     public static void main(String[] args) {
+        String s1 = "Shape";
+        String s2 = "Phase";
+
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+
+        if (s1.length() == s2.length()) {
+            char[] charArray1 = s1.toCharArray();
+            char[] charArray2 = s2.toCharArray();
+
+            Arrays.sort(charArray1);
+            Arrays.sort(charArray2);
+
+            boolean result = Arrays.equals(charArray1, charArray2);
+
+            if (result) {
+                System.out.println(s1 + " and " + s2 + " are an anagram.");
+            } else {
+                System.out.println(s1 + " and " + s2 + " are not an anagram.");
+            }
+        } else {
+            System.out.println(s1 + " and " + s2 + " are not an anagram.");
+
+        }
+    }
+}
 
         //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
         //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
-    }
-}
